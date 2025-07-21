5c1d2753082e587cbbcad072d4f5143d8f86b48c
Masi <mas.ahmadii@yahoo.com>
2025-07-21 10:34:46 +0330

    set up UserContext

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useReducer } from "react";
+import { initialState, userReducer } from "@/reducer/userReducer"; 
+ 
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(userReducer, initialState);
+
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
